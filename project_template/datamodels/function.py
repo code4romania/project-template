19aeababd5c038a2eb95632d64e0d46b64567d21
radu diaconescu <diaconescuradu13@gmail.com>
2018-04-01 22:52:08 +0300

    added enums for declaration type, function, institute. Resolves CP-39,CP-40,CP-42

---
+++ project_template/datamodels/function.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Function(Enum):
+    GENERAL_SECRETARY = "Secretar General"
+    DEPUTY = "Deputat"
+    SENATOR = "Senator"
+    PRIME_MINISTER = "Prim Ministru"
+    VICE_PRIME_MINISTER = "Vice Prim Ministru"
+    MINISTER = "Minister"
---
+++ project_template/datamodels/institute.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Institute(Enum):
+    GOVERNMENT = "Guvernul Romaniei"
+    PARLIAMENT = "Parlamentul Romaniei "
+    SENAT = "Senat"
+    DEPUTIES_CHAMBER = "Camera Deputatilor"
+
+
